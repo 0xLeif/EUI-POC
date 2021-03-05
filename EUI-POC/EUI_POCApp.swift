124d0416f1226b47042697ed91c1a2a2a214d918
Zach Eriksen <zmeriksen@icloud.com>
2021-03-05 16:57:49 -0600

    Initial Commit

---
+++ EUI-POC/EUI_POCApp.swift
@@ -0,0 +1,17 @@
+//
+//  EUI_POCApp.swift
+//  EUI-POC
+//
+//  Created by Leif on 3/5/21.
+//
+
+import SwiftUI
+
+@main
+struct EUI_POCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
