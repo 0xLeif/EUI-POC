34419a65fd4fc92b70971ccad8dca5deeec69b5c
Zach Eriksen <zmeriksen@icloud.com>
2021-03-05 19:28:41 -0600

    basic poc

--- EUI-POC/EUI_POCApp.swift
+++ EUI-POC/EUI_POCApp.swift
@@ -6,12 +6,40 @@
 //
 
 import SwiftUI
+import EUI
+import ScreenData
 
 @main
 struct EUI_POCApp: App {
     var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+        EUIAppScene(
+            app: EUIApp(
+                initialScreen: EUIScreen(
+                    id: "initial",
+                    title: "Initial Screen!",
+                    backgroundColor: .green,
+                    headerView: nil,
+                    bodyView: .button(SomeButton(title: "Hello World!", actionID: "print")),
+                    footerView: .label(
+                        SomeLabel(
+                            title: "Some Footer Label",
+                            font: .title,
+                            destination: Destination(type: .screen, toID: "another")
+                        )
+                    )
+                ),
+                screens: [
+                    EUIScreen(
+                        id: "another",
+                        title: "Another Screen!",
+                        backgroundColor: .green,
+                        bodyView: .button(SomeButton(title: "Hello World?"))
+                    )
+                ],
+                actions: [
+                    EUIAction(id: "print", action: .complete(.void { print("Hello, World!")}))
+                ]
+            )
+        )
     }
 }
